dc5029e92f5f8252f66080233ae11cc15738f34b
Naoki_Sawada <appdev1407@gmail.com>
2017-02-15 22:06:49 +0900

    initial project

---
+++ VIPERForObjective-C/VIPERForObjective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VIPERForObjective-C
+//
+//  Created by Naoki_Sawada on 2017/02/15.
+//  Copyright © 2017年 viperObjC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VIPERForObjective-C/VIPERForObjective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VIPERForObjective-C
+//
+//  Created by Naoki_Sawada on 2017/02/15.
+//  Copyright © 2017年 viperObjC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
